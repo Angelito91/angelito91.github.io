4b237ab0d765cdaf73493b6d8e8c4a010f8fb8a0
Angelito91 <portuondoangel@gmail.com>
2024-03-15 14:17:55 -0400

    feat: new version 3.0.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://angelito91.github.io/',
-  base: '/portfolio'
+  base: '/'
 });
